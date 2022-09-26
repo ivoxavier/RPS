a75796bf44864220532efb8e9e5a351087b76da2
ivoxavier <ivofernandes12@gmail.com>
2022-09-26 16:51:21 +0100

    remove build dir
    remove build dir

--- build/all/app/CMakeFiles/rps.desktop.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rps.desktop"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rps.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
